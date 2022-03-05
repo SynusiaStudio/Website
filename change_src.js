d6a1108ca36ec0a1b5ffd3ba53072d110bf000cf
SynusiaStudio <synusiastudio@protonmail.com>
2022-03-05 21:43:21 +0100

    Update

---
+++ change_src.js
@@ -0,0 +1,11 @@
+$('.container').onload(function(){
+		$("a").click(function(e) {
+			e.preventDefault();
+			$('#youtube').attr("src", $(this).attr("value"));
+		})
+	});
+	
+	
+	getElementById('iframe'), setAttribute('src')
+	
+	var value = document.getElementById("theInput").innerHTML;
