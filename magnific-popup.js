1dce02a3827339769df6cfc757471f4093c4d5bb
SynusiaStudio <synusiastudio@protonmail.com>
2022-03-05 23:43:18 +0100

    Update

--- magnific-popup.js
+++ magnific-popup.js
@@ -1,7 +1,127 @@
-$('.container').on('click', function () {
+$('#1').on('click', function () {
     $.magnificPopup.open({
         items: {
-            src: '<iframe width="560" height="315" src="https://www.youtube.com/embed/%id%" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/IfQXMQiTyPo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#2').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/C8bdoSHKRII" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#3').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/uMPT7JoDNxI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#4').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/oRZNa7TNyxo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#5').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/kQ6z4ryZt6M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#6').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/UOzxVoHJjGE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#7').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/-W6vxN33YMc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#8').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/GP9-D56r-ao" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#9').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/API59wtyDws" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#10').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/Jqp019V9z1U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#11').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/jFWCZOL1wao" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#12').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/oOS_Vq9pZ-s" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#13').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/ACwKF30Nm7U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#14').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/47WYdVfS3Ak" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#15').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/LMvWjTAffR0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }
+    });
+});
+
+$('#16').on('click', function () {
+    $.magnificPopup.open({
+        items: {
+            src: '<iframe width="1024" height="576" margin:auto src="https://www.youtube.com/embed/uF2gOcm2p8E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
         }
     });
 });
